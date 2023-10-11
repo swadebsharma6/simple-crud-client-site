1689fc47299deb20e16ed979c8847578d88db1a3
swadebsharma6 <swadebriva2012@gmail.com>
2023-10-12 04:13:17 +0600

    implement delete oparation

--- src/Pages/About/User.jsx
+++ src/Pages/About/User.jsx
@@ -1,11 +1,31 @@
 import PropTypes from 'prop-types';
 
 const User = ({user}) => {
-    const {name, email} = user;
+    const {_id,name, email} = user;
+
+
+    const handleDelete = (id) =>{
+
+        console.log('delete', id);
+        fetch(`http://localhost:5000/users/${id}`,{
+            method: "DELETE"
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.deletedCount>0){
+                alert('Deleted Successfully')
+            }
+        })
+
+    }
+
+
     return (
         <div className="border bg-blue-400 text-white rounded-xl p-4 ">
             <h3>Name: {name}</h3>
             <p>Email: {email}</p>
+            <button onClick={()=> handleDelete(_id)} className='btn btn-sm btn-warning'>Delete-X</button>
         </div>
     );
 };
